d46cdf29cda878ca2c1f63b09e5231aaa42eb729
Giovani M. <giovanimiamoto24@gmail.com>
2025-04-24 10:25:15 -0300

    teste

---
+++ LP_LISTA02/exercicio02/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Digite um número: ");
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        System.out.println("O quadrado do número digitado é: " + (number * number));
+    }
+}
